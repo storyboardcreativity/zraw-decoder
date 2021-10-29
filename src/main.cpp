866a8b43faafd3cdc3aa0de44e7d4c108182574b
xhrust <storyboardcreativity@gmail.com>
2021-10-29 13:31:15 -0400

    Saving packed decoded values to DNG + color matrix
    - Fixed "tabs" in file (converted '\t' symbols to spaces)
    - Now ColorMatrix1 is set to CIE XYZ -> sRGB conversion, D65
    - Now WhiteLevel is set (to max value according to bpp)
    - Now AnalogBalance is set (to 1,1,1)
    - Now DNGVersion is set (to 1.2.0.0)
    - Now CFARepeatPatternDim is set (to 2x2)
    - Now CFAPattern is set (to RGGB)
    - Now pixel values are correctly packed to DNG according to bits_per_photodiode_value

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,9 @@ int main(int argc, char** argv)
     Args args {argc, argv};
     if (args.verbose)
     {
-		std::cout << "Input file: " << args.input << std::endl;
+        std::cout << "Input file: " << args.input << std::endl;
         std::cout << "Output file: " << args.output << std::endl;
-	}
+    }
 
     // Open file and get size
     std::ifstream file(args.input, std::ios::binary | std::ios::ate);
@@ -59,12 +59,6 @@ int main(int argc, char** argv)
     // Decode frame
     auto decompression_state = zraw_decoder__decompress_hisi_frame(decoder);
 
-    // Release ZRAW decoder
-    zraw_decoder__free(decoder);
-
-
-
-
     tinydngwriter::DNGImage dng_image;
     dng_image.SetBigEndian(false);
 
@@ -73,8 +67,11 @@ int main(int argc, char** argv)
     dng_image.SetImageLength(frame_info.height_in_photodiodes);
     dng_image.SetRowsPerStrip(frame_info.height_in_photodiodes);
     dng_image.SetSamplesPerPixel(1);
+
+    // Bits Per Photodiode value
     uint16_t bps[1] = {frame_info.bits_per_photodiode_value};
     dng_image.SetBitsPerSample(1, bps);
+
     dng_image.SetPlanarConfig(tinydngwriter::PLANARCONFIG_CONTIG);
     dng_image.SetCompression(tinydngwriter::COMPRESSION_NONE);
     dng_image.SetPhotometric(tinydngwriter::PHOTOMETRIC_CFA);
@@ -83,12 +80,45 @@ int main(int argc, char** argv)
     dng_image.SetResolutionUnit(tinydngwriter::RESUNIT_NONE);
     dng_image.SetImageDescription("[Storyboard Creativity] ZRAW -> DNG converter generated image.");
 
-    std::vector<uint16_t> image_data(frame_info.width_in_photodiodes * frame_info.height_in_photodiodes);
-    for (int y = 0; y < frame_info.height_in_photodiodes; ++y)
-        for (int x = 0; x < frame_info.width_in_photodiodes; ++x)
-            image_data[y * frame_info.width_in_photodiodes + x] = rand();
+    dng_image.SetUniqueCameraModel("Z CAM E2");
 
-    dng_image.SetImageData(reinterpret_cast<unsigned char *>(image_data.data()), image_data.size() * sizeof(uint16_t));
+    // CM (we set standard CIE XYZ -> sRGB matrix, D65)
+    double matrix1[] =
+    {
+        3.2404542, -1.5371385, -0.4985314,
+        -0.9692660, 1.8760108, 0.0415560,
+        0.0556434, -0.2040259, 1.0572252
+    };
+    dng_image.SetColorMatrix1(3, matrix1);
+
+    // AWB
+    double analog_balance[] = {1, 1, 1};
+    dng_image.SetAnalogBalance(3, analog_balance);
+
+    // Black Levels
+    dng_image.SetBlackLevel(4, frame_info.cfa_black_levels);
+
+    dng_image.SetDNGVersion(1, 2, 0, 0);
+
+    dng_image.SetCFARepeatPatternDim(2, 2);
+
+    uint8_t cfa_pattern[4] = {0, 1, 1, 2};
+    dng_image.SetCFAPattern(4, cfa_pattern);
+
+    double white_levels[1] = {((1 << frame_info.bits_per_photodiode_value) - 1)};
+    dng_image.SetWhiteLevelRational(1, white_levels);
+
+    // Get CFA data
+    std::vector<uint16_t> image_data(frame_info.width_in_photodiodes * frame_info.height_in_photodiodes);
+    auto cfa_state = zraw_decoder__get_decompressed_CFA(decoder, image_data.data(), image_data.size() * sizeof(uint16_t));
+
+    for (int i = 0; i < image_data.size(); ++i)
+        if(image_data[i] > 0x0FFF) image_data[i] = 0x0FFF;
+
+    // Release ZRAW decoder
+    zraw_decoder__free(decoder);
+   
+    dng_image.SetImageDataPacked(image_data.data(), image_data.size(), frame_info.bits_per_photodiode_value, true);
 
     tinydngwriter::DNGWriter dng_writer(false);
     dng_writer.AddImage(&dng_image);
